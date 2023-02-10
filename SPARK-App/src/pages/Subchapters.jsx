26a6c558e6b4d79d3976a3dd17871b95061be5fa
Lin Peishan <linpeishann@gmail.com>
2023-02-10 20:57:47 +0800

    Update Subchapters.jsx

--- SPARK-App/src/pages/Subchapters.jsx
+++ SPARK-App/src/pages/Subchapters.jsx
@@ -7,6 +7,7 @@ import AddIcon from '@mui/icons-material/Add'
 import SubchapterCard from '../components/subchapters/SubchapterCard'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Subchapters = () => {
     const navigate = useNavigate();
