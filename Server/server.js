c54a186dd27bc9f29cfa3d882e914a39fcd86d6f
Lin Peishan <linpeishann@gmail.com>
2023-02-12 19:09:47 +0800

    Forgot to upload server.js file

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,6 @@ import crypto from 'crypto'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 
-import { PrismaClient } from '@prisma/client'
 import { uploadFile, deleteFile, getObjectSignedUrl } from './s3.js'
 import chapterRouter from './routes/chapterRouter.js'
 import { connectDB } from './config/db.js'
@@ -14,12 +13,11 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 connectDB();
-// const prisma = new PrismaClient()
 
-// const storage = multer.memoryStorage()
-// const upload = multer({ storage: storage })
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
-// const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex')
+const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -38,8 +36,6 @@ app.get('/health', async (req, res) => {
   }
 })
 
-
-
 // app.get("/api/posts", async (req, res) => {
 //   const posts = await prisma.posts.findMany({orderBy: [{ created: 'desc'}]})
 //   for (let post of posts) {
@@ -49,31 +45,22 @@ app.get('/health', async (req, res) => {
 //   res.send(posts)
 // })
 
-// app.post('/api/posts', upload.single('image'), async (req, res) => {
+app.post('/api/posts', upload.single('image'), async (req, res) => {
 
-//   console.log("req.body: ",req.body);
-//   console.log("req.file: ",req.file); // image obj
-//   // req.file.buffer --> image send to S3
-//   const file = req.file
-//   const caption = req.body.caption
-//   const imageName = generateFileName()
+  const file = req.file
+  const caption = req.body.caption
+  const imageName = generateFileName()
 
-//   const fileBuffer = await sharp(file.buffer)
-//     .resize({ height: 1920, width: 1080, fit: "contain" })
-//     .toBuffer() //can delete this later
+  const fileBuffer = await sharp(file.buffer)
+    .toBuffer() //can delete this later
 
-//   await uploadFile(fileBuffer, imageName, file.mimetype)
-//   // **** try to return the object URL
-//   const post = await prisma.posts.create({
-//     data: {
-//       imageName,
-//       caption,
-//     }
-//   })
+  await uploadFile(fileBuffer, imageName, file.mimetype).then( (i) => {
+    console.log("Upload image done! ",imageName);
+    console.log("Upload image done! ImageUrl: https://testing-bucket-clt.s3.us-east-1.amazonaws.com/"+imageName);
+    res.send({'location': "https://testing-bucket-clt.s3.us-east-1.amazonaws.com/"+ imageName})
+  })
   
-//   res.status(201).send(post)
-// })
-
+})
 // app.delete("/api/posts/:id", async (req, res) => {
 //   const id = +req.params.id
 //   const post = await prisma.posts.findUnique({where: {id}}) 
