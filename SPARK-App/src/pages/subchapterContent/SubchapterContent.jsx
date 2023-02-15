a5a03948e1a3f36ff4346c04410020b0202bba3a
natalie <sachua.2020@smu.edu.sg>
2023-02-15 15:20:53 +0800

    changes as per pull request comments

--- SPARK-App/src/pages/subchapterContent/SubchapterContent.jsx
+++ SPARK-App/src/pages/subchapterContent/SubchapterContent.jsx
@@ -5,6 +5,7 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import "./subchapterContent.css";
 import { Tooltip } from '@mui/material';
+import axios from 'axios';
 
 const API_URL = "http://localhost:8080/chapters"
 
@@ -14,19 +15,17 @@ const SubchapterContent = () => {
     const [chapter, setChapter] = useState([]);
 
     const getChapterContent = async (chapterId) => {
-        const response = await fetch(`${API_URL}/${chapterId}`)
-        const data = await response.json()
-
-        console.log(data)
-        setChapter(data)
+        axios.get(`${API_URL}/${chapterId}`)
+        .then(res => {
+            setChapter(res.data)
+        })
     }
 
     const getSubchapterContent = async (chapterId, subchapterId) => {
-        const response = await fetch(`${API_URL}/${chapterId}/subchapters/${subchapterId}`)
-        const data = await response.json()
-
-        console.log(data)
-        setSubchapter(data)
+        axios.get(`${API_URL}/${chapterId}/subchapters/${subchapterId}`)
+        .then(res => {
+            setSubchapter(res.data)
+        })
     }
 
     useEffect(() => {
@@ -69,44 +68,6 @@ const SubchapterContent = () => {
                 </div>
                 <div className="subchapterContentBottom">
                     <div className="subchapterContentBody" dangerouslySetInnerHTML={{__html: subchapter.content}}>
-                        {/* {subchapter.content} */}
-                        {/* <h2>🔍 Indications</h2>
-                        <p>Haemodynamically stable <b>and</b> <br/>
-
-                        1. Salvageable severe head injury with <br/>
-
-                        2. Intracranial hypertension <br/>
-
-                        3. Refractory to maximal medical and surgical intra-cranial pressure lowering therapies.</p>
-                        <br/>
-                        <br/>
-                        <h2>👨‍🔬 Initiation and Titration</h2>
-                        <ul>
-                            <li>
-                                Dilute thiopentone to 25mg/ml by adding 20ml of sterile water to each 500 mg bottle of thiopentone
-                            </li>
-                            <li>
-                                Loading dose: Give an initial bolus of 250 mg over 10-20 min. This may be repeated up to a total load dose of 500-1000 mg, according to ICP response or attainment of burst suppression on the EEG tracing.
-                            </li>
-                            <li>
-                                Maintenance dose: 125-500 mg/hr titrated to ICP control or maintenance of burst suppression EEG
-                            </li>
-                            <li>
-                                Stop Morphine and other sedation
-                            </li>
-                            <br/>
-                        </ul>
-                        <p>Once ICP control has been achieved for 24-36 hrs, the thiopentone infusion may be reduced gradually and stopped. Monitor for rebound in the ICP, which may require restarting the thiopentone infusion.</p>
-                        <br/>
-                        <br/>
-                        <h2>🙅‍♀️ Complications</h2>
-                        <ol>
-                            <li>Hypokalemia during induction of barbituate coma: Check UECr Q6H</li>
-                            <li>Hypokalemia during induction of barbituate coma: Check UECr Q6H</li>
-                            <li>Hypokalemia during induction of barbituate coma: Check UECr Q6H</li>
-                            <li>Hypokalemia during induction of barbituate coma: Check UECr Q6H</li>
-                            <li>Hypokalemia during induction of barbituate coma: Check UECr Q6H</li>
-                        </ol> */}
                     </div>
                 </div>
             </div>
