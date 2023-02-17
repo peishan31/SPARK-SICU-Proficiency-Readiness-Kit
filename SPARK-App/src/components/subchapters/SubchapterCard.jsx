ef8fa60bedc7ff3bb9ae8cb85277163191569985
7linli <7linli@users.noreply.github.com>
2023-02-17 22:22:37 +0800

    changes on subchaptercard component and subchapter page

--- SPARK-App/src/components/subchapters/SubchapterCard.jsx
+++ SPARK-App/src/components/subchapters/SubchapterCard.jsx
@@ -1,15 +1,34 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea, IconButton, Box, Grid } from '@mui/material';
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import { useState } from 'react';
+import * as React from 'react'
+import { Typography, Card, CardContent, CardMedia, CardActionArea, IconButton, Box, Grid } from '@mui/material'
+import { useState } from 'react'
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder'
+import BookmarkIcon from '@mui/icons-material/Bookmark'
+import axios from 'axios'
+
 
 export default function MultiActionAreaCard({ subchapter }) {
-    const [clicked, setClicked] = useState(false)
+    const currentSubchapterId = subchapter._id;
+    const [clicked, setClicked] = useState('')
+    async function addBookmark() {
+        await axios.put(
+            'http://localhost:8080/user/63e87a7780b6c0bcb29d15d0/bookmarks/',
+            {
+                subchapterId: currentSubchapterId,
+                chapterId: "63ea35d26c0ef100ca017647"
+            }
+        );
+    }
+
+    async function removeBookmark() {
+        await axios.delete(
+            'http://localhost:8080/user/63e87a7780b6c0bcb29d15d0/bookmarks/63ec93ea8da3af3690c762d5',
+            {
+                subchapterId: currentSubchapterId,
+                chapterId: "63ea35d26c0ef100ca017647"
+            }
+        );
+    }
+
     return (
         <Card sx={{ maxWidth: 445 }}>
             <CardActionArea>
@@ -22,11 +41,19 @@ export default function MultiActionAreaCard({ subchapter }) {
                 <CardContent>
                     <Grid pb={1} display="flex" alignItems="center">
                     <Typography display="contents" gutterBottom variant="h5" component="div">
-                        {subchapter.title}
+                        {subchapter.subchapterTitle}
                     </Typography>
                     <Box ml="auto">
-                    <IconButton color="primary" onClick={e => {setClicked(!clicked)}}>
-                        {clicked ? <BookmarkIcon /> : <BookmarkBorderIcon /> }
+                    <IconButton color="primary" onClick={
+                        e => {setClicked(!clicked)
+                            if(clicked) {
+                                console.log("yes")
+                            }else{
+                                addBookmark()
+                            }
+                        }}>
+                        {clicked ? <BookmarkIcon /> : <BookmarkBorderIcon /> 
+                        }
                     </IconButton>
                     </Box>
                     </Grid>
--- SPARK-App/src/pages/Subchapters.jsx
+++ SPARK-App/src/pages/Subchapters.jsx
@@ -1,58 +1,41 @@
 import React from 'react'
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
 import AddIcon from '@mui/icons-material/Add'
 import SubchapterCard from '../components/subchapters/SubchapterCard'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
-import { useNavigate } from 'react-router-dom'
+import Box from '@mui/material/Box'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Subchapters = () => {
-    const navigate = useNavigate();
+    const location = useLocation();
 
-    const navigateToSubChapter = () => {
-        navigate('/createsubchapter');
-    }
-    const subchapters = [
-        {
-            title: "Subchapter 1",
-            description: "This is the first subchapter",
-            image: "../../src/handbook1.jpg"
-
-        },
-        {
-            title: "Subchapter 2",
-            description: "This is the second subchapter",
-            image: "../../src/handbook1.jpg"
-        },
-        {
-            title: "Subchapter 3",
-            description: "This is the third subchapter",
-            image: "../../src/handbook1.jpg"
-        },
-        {
-            title: "Subchapter 3",
-            description: "This is the third subchapter",
-            image: "../../src/handbook1.jpg"
-        },
-        {
-            title: "Subchapter 3",
-            description: "This is the third subchapter",
-            image: "../../src/handbook1.jpg"
-        },
-    ]
-
+    const chapterId = location.state.parentChapterId
+    
+    const [subchapters, setSubchapters] = useState([]);
+    useEffect(() => {
+        axios.get(`http://localhost:8080/chapters/${chapterId}/subchapters`)
+            .then(res => {
+                setSubchapters(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
 
     return (
-        <div>
+        <Box margin={3} >
             <Grid pb={2} display="flex" alignItems="center">
                 <Typography variant="h4">Subchapters</Typography>
                 <Stack direction="row" spacing={2} ml="auto">
                     <Button variant="outlined">Select</Button>
-                    <Button variant="outlined" onClick={navigateToSubChapter}>
+                    {/* <Button variant="outlined" onClick={navigateToSubChapter}>
                         <AddIcon />
                             Create new subchapter
-                    </Button>
+                    </Button> */}
                 </Stack>
             </Grid>
             <Grid container spacing={4}>
@@ -66,7 +49,7 @@ const Subchapters = () => {
                 })
             }
             </Grid>
-        </div>
+        </Box>
     )
 }
 
