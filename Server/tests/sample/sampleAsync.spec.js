874a6d2a6773c11a0330bfea109a85c60b0657ca
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2023-02-26 14:17:01 +0800

    added sample jest files for reference

---
+++ Server/tests/sample/sampleAsync.spec.js
@@ -0,0 +1,63 @@
+const axios = require('axios')
+
+const fetchData = async (id) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    return response.data;
+};
+
+describe("Testing async get function", () => {
+    it ("should return data for id 1", async () => {
+        const expectedResult = {
+            userId: 1,
+            id: 1,
+            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        }
+        const data = await fetchData(1);
+        expect(data).toEqual(expectedResult);
+    })
+    
+    it ("should return data for user id 2", async () => {
+        const expectedResult = {
+            userId: 1,
+            id: 2,
+            title: "qui est esse",
+            body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+        }
+        const data = await fetchData(2);
+        expect(data).toEqual(expectedResult);
+    })
+})
+
+
+// setting up and tearing down
+let animals = [];
+
+
+beforeAll(() => {
+    console.log("before all");
+    animals = ["dog", "cat", "bird"];
+})
+
+describe("animals array", () => {
+    // runs before each it test, this applies to all it tests in this describe block
+    beforeEach(() => {
+        console.log("before each");
+        animals = ["dog", "cat", "bird"];
+    })
+
+    it ("Should add animals to end of array", () => {
+        animals.push("fish");
+        expect(animals).toContain("fish");
+    })
+
+    it ("Should have initial length of 3", () => {
+        expect(animals.length).toBe(3);
+    })
+})
+
+
+afterAll(() => {
+    console.log("after all");
+    animals = ["dog", "cat", "bird"];
+})
