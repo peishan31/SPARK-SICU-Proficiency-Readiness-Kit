4b65670d4d8d56d04f4b583b1869e1e7af80d6c3
Lin Peishan <linpeishann@gmail.com>
2023-03-02 02:22:33 +0800

    Update App.jsx

--- SPARK-App/src/App.jsx
+++ SPARK-App/src/App.jsx
@@ -17,9 +17,9 @@ import Login from './pages/login/Login'
 function App() {
   const [token, setToken] = useState()
 
-  // if (!token) {
-  //   return <Login setToken={setToken}/>
-  // }
+  if (!token) {
+    return <Login setToken={setToken}/>
+  }
 
   return (
     <>
