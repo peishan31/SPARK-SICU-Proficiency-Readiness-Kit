cd980d7c38e620fa0be8c859163a31b599ad138d
7linli <7linli@users.noreply.github.com>
2023-03-12 19:37:20 +0800

    created responsive apache score calculator page

--- SPARK-App/src/pages/viewCalculator/viewApacheIIScoreCalculator.jsx
+++ SPARK-App/src/pages/viewCalculator/viewApacheIIScoreCalculator.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from '@mui/material'
+import { Button, ToggleButton, ToggleButtonGroup, Paper, Divider, styled } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
@@ -23,17 +23,337 @@ const ApacheIIScore = () => {
         // console.log(formValues);
       };
 
+    const Item = styled(Paper)(({ theme }) => ({
+        padding: theme.spacing(1),
+        textAlign: 'left',
+        boxShadow: 'none',
+    }));
+
+    const [fiOxygen, setFiOxygen] = React.useState('');
+
+    const handleChange = (e, newFiOxygen) => {
+      setFiOxygen(newFiOxygen);
+      if(newFiOxygen == 'lessThan') {
+          document.getElementById('paOxygen').style.display = "flex";
+          document.getElementById('aaGradient').style.display = "none";
+    }
+      else if(newFiOxygen == 'moreOrEqualTo') {
+          document.getElementById('aaGradient').style.display = "flex";
+          document.getElementById('paOxygen').style.display = "none";
+      } else {
+          document.getElementById('paOxygen').style.display = "none";
+          document.getElementById('aaGradient').style.display = "none";
+      }
+    };
+
     const tabs = [
         {
           label: "General Information",
           Component: (
             <form onSubmit={handleSubmit}>
-                <Grid container alignItems="center" justify="center" direction="column" style={{margin: '50px'}}>
-                    <Grid item>calculator fields here</Grid>
-                    <Button variant="contained" color="primary" type="submit">
-                    Submit
-                    </Button>
-                </Grid>
+                {/* <Box component="form" sx={{'& .MuiTextField-root': { m: 1, width: '25ch' },}}
+                    noValidate autoComplete="off"
+                >
+
+                </Box> */}
+                <Box sx={{ flexGrow: 1 }}>
+                    <Grid container spacing={2} justifyContent="center" alignItems="center">
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <Typography>
+                                    Age
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <TextField label="Years" variant="outlined" />
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <Typography>
+                                    Temperature
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <TextField label="°C" variant="outlined" />
+                                {/* <Button variant="outlined" sx={{ml: 1, fontSize: 25}}>&#128177;</Button> */}
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Divider></Divider>
+                    <Grid container spacing={2} justifyContent="center" alignItems="center">
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <Typography>
+                                    Mean arterial pressure
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <TextField label="mm Hg" variant="outlined" />
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <Typography>
+                                    pH
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <TextField label="pH" variant="outlined" />
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Divider></Divider>
+                    <Grid container spacing={2} justifyContent="center" alignItems="center">
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <Typography>
+                                    Heart rate/pulse
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <TextField label="beats/min" variant="outlined" />
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <Typography>
+                                    Respiratory rate
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <TextField label="breaths/min" variant="outlined" />
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Divider></Divider>
+                    <Grid container spacing={2} justifyContent="center" alignItems="center">
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <Typography>
+                                    Sodium
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <TextField label="mmol/L" variant="outlined" />
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <Typography>
+                                    Potassium
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <TextField label="mmol/L" variant="outlined" />
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Divider></Divider>
+                    <Grid container spacing={2} justifyContent="center" alignItems="center">
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <Typography>
+                                    Creatinine
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <TextField label="µmol/L" variant="outlined" />
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <Typography>
+                                    Hematocrit
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <TextField label="%" variant="outlined" />
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Divider></Divider>
+                    <Grid container spacing={2} justifyContent="center" alignItems="center">
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <Typography>
+                                    White blood cell count
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <TextField label="× 10⁹ cells/L" variant="outlined" />
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <Typography>
+                                    Glasgow Coma Scale
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <Item>
+                                <TextField label="points" variant="outlined" />
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Divider></Divider>
+                    <Grid container spacing={2} justifyContent="center" alignItems="center">
+                        <Grid item xs={12} sm={6}>
+                            <Item>
+                                <Typography>
+                                    History of severe organ failure or immunocompromise
+                                </Typography>
+                                <Typography variant="caption">
+                                    Heart Failure Class IV, cirrhosis, chronic lung disease, or dialysis-dependent
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Item>
+                                <ToggleButtonGroup exclusive>
+                                    <ToggleButton value="yes">
+                                        Yes
+                                    </ToggleButton>
+                                    <ToggleButton value="no">
+                                        No
+                                    </ToggleButton>
+                                </ToggleButtonGroup>
+                            </Item>
+                        </Grid>
+                        </Grid>
+                    <Divider></Divider>
+                    <Grid container spacing={2} justifyContent="center" alignItems="center">
+                        <Grid item xs={12} sm={6}>
+                            <Item>
+                                <Typography>
+                                    Acute renal failure
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Item>
+                                <ToggleButtonGroup exclusive>
+                                    <ToggleButton value="yes">
+                                        Yes
+                                    </ToggleButton>
+                                    <ToggleButton value="no">
+                                        No
+                                    </ToggleButton>
+                                </ToggleButtonGroup>
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Divider></Divider>
+                    <Grid container spacing={2} justifyContent="center" alignItems="center">
+                        <Grid item xs={12} sm={6}>
+                            <Item>
+                                <Typography>
+                                    FiO₂
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Item>
+                                <ToggleButtonGroup color="primary" value={fiOxygen} exclusive onChange={handleChange}>
+                                    <ToggleButton value="lessThan">
+                                        &lt;50% (or non-intubated)
+                                    </ToggleButton>
+                                    <ToggleButton value="moreOrEqualTo">
+                                        ≥50%
+                                    </ToggleButton>
+                                </ToggleButtonGroup>
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Divider></Divider>
+                    <Grid id="paOxygen" container spacing={2} justifyContent="center" alignItems="center" style={{display:'none'}}>
+                        <Grid item xs={12} sm={6}>
+                            <Item>
+                                <Typography>
+                                    PaO₂, mmHg
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Item>
+                                <ToggleButtonGroup exclusive>
+                                    <ToggleButton value="">
+                                        &gt; 70
+                                    </ToggleButton>
+                                    <ToggleButton value="">
+                                        61 - 70
+                                    </ToggleButton>
+                                    <ToggleButton value="">
+                                        55 - 60
+                                    </ToggleButton>
+                                    <ToggleButton value="">
+                                        &lt; 55
+                                    </ToggleButton>
+                                </ToggleButtonGroup>
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Divider></Divider>
+                    <Grid id="aaGradient" container spacing={2} justifyContent="center" alignItems="center" style={{display:'none'}}>
+                        <Grid item xs={12} sm={6}>
+                            <Item>
+                                <Typography>
+                                    A-a gradient
+                                </Typography>
+                            </Item>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <Item>
+                                <ToggleButtonGroup exclusive>
+                                    <ToggleButton value="">
+                                        &lt; 200
+                                    </ToggleButton>
+                                    <ToggleButton value="">
+                                        200 - 349
+                                    </ToggleButton>
+                                    <ToggleButton value="">
+                                        350 - 499
+                                    </ToggleButton>
+                                    <ToggleButton value="">
+                                        &gt; 499
+                                    </ToggleButton>
+                                </ToggleButtonGroup>
+                            </Item>
+                        </Grid>
+                    </Grid>
+                    <Divider></Divider>
+                    <div>
+                        <Button variant="contained" sx={{m: 2}} color="primary" type="submit">
+                            Reset
+                        </Button>
+                        <Button variant="contained" sx={{m: 2}} color="primary" type="submit">
+                            Calculate
+                        </Button>
+                    </div>
+                </Box>
             </form>
             
           )
