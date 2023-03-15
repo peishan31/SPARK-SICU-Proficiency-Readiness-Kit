fa5b99a93cca812417a962d04be38a1a818d99e6
natalie <sachua.2020@smu.edu.sg>
2023-03-15 17:14:15 +0800

    upsert user

--- Server/routes/userRouter.js
+++ Server/routes/userRouter.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import bookmarkRouter from './bookmarkRouter.js';
 import User from '../models/UserModel.js';
+import { OAuth2Client } from 'google-auth-library'
+
 const userRouter = express.Router();
+
+// Google Auth
+const client = new OAuth2Client(process.env.SSO_CLIENT_ID);
 
 // @description: Get health status of user route
 // @route GET user/health
@@ -17,55 +22,73 @@ userRouter.get("/health", async (req, res) => {
 
 // @description: Authenticate user
 // @route POST user/login
-userRouter.post("/login", async (req, res) => {
-  try{
-    const { email, password } = req.body;
-    const user = await User.findOne({ email: email }); 
-
-    if (user) {
-      // check password
-      if (user.password == password) {
-        return res.status(200).json({
-          _id: user._id,
-          email: user.email,
-          userType: user.userType
-        });
-      }
-      return res.status(400).json({
-        msg: 'Invalid Login Credentials'
+userRouter.post('/login', (req,res)=>{
+  console.log("login route accessed")
+  let token = req.body.token;
+  let user = {}
+  
+  async function verify() {
+      const ticket = await client.verifyIdToken({
+          idToken: token,
+          audience: process.env.SSO_CLIENT_ID, 
       });
-    }
+      const payload = ticket.getPayload();
+      const userId = payload.sub;
 
-    return res.status(400).json({
-      msg: 'Invalid Login Credentials'
-    });
+      let user = await User.findOne({ googleId: userId });
 
-  } catch (err) {
-    console.error(err.message)
-    res.status(500).send('Server Error')
+      if (!user) {
+        user = new User({
+          googleId: payload.sub,
+          name: payload.name,
+          email: payload.email,
+          picture: payload.picture,
+          userType: "junior"
+        })
+      } else {
+        // If the user already exists, update their information
+        user.name = payload.name;
+        user.email = payload.email;
+        user.picture = payload.picture;
+      }
+
+      await user.save();
+
+      return user;
   }
-});
+  
+  verify()
+  .then(user => {
+      console.log(token)
+
+      res.cookie('session-token', token);
+      res.send(user);
+  })
+  .catch(
+    console.error
+    );
+})
 
 
-// @description: Create user
-// @route POST user/register
-userRouter.post("/register", async (req, res) => { 
+// // @description: Create user
+// // @route POST user/register
+// userRouter.post("/register", async (req, res) => { 
 
-  try {
-    const { email, userType, password } = req.body;
-    const newUser = new User({
-      email,
-      userType,
-      password
-    });
+//   try {
+//     const { email, userType, password } = req.body;
+//     const newUser = new User({
+//       email,
+//       userType,
+//       password
+//     });
 
-    const user = await newUser.save();
-    return res.status(201).json(user);
-  } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error')
-    }
-});
+//     const user = await newUser.save();
+//     return res.status(201).json(user);
+//   } catch (err) {
+//         console.error(err.message);
+//         res.status(500).send('Server Error')
+//     }
+// });
 
 /*
 // link to subchapter route
