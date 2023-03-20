99ec8644480e9dd33e4a8bc7cc15aa9aaa28dfa5
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2023-03-20 10:53:13 +0800

    bookmarks test only works for googleIds without bookmarks for now

--- Server/tests/bookmarks/bookmarks.spec.js
+++ Server/tests/bookmarks/bookmarks.spec.js
@@ -1,52 +1,53 @@
-describe("Testing objects ", () => {
-    it("object assignment", () => {
-        const data = { one: 1 };
-        data["two"] = 2;
-        expect(data).toEqual({ one: 1, two: 2 });
-    })
-})
-
-
-// const axios = require('axios')
-
-// const NO_BOOKMARKS_USER_ID = "63e87aed80b6c0bcb29d15d4"
-// const WITH_BOOKMARKS_USER_ID = "63fb1b9e40628116ea970e57"
-// const CHAPTER_ID = "6415b83df79d4564b394fbea"
-// const API_URL = "https://spark-sicu-proficiency-readiness-kit-backend.vercel.app"
-
-// const getBookmarks = async (userId) => {
-//     const response = await axios.get(API_URL + `/user/${userId}/bookmarks`);
-//     return response;
-// };
-
-// const getAllSubchaptersForAChapter = async (userId, chapterId) => {
-//     const response = await axios.get(API_URL + `/user/${userId}/bookmarks/chapters/${chapterId}`);
-//     return response;
-// };
-
-// const addBookmarkForASubchapter = async (userId, data) => {
-//     const response = await axios.put(API_URL + `/user/${userId}/bookmarks/`, data);
-//     return response;
-// };
-
-// const deleteBookmarkForASubchapter = async (userId, bookmarkId) => {
-//     const response = await axios.delete(API_URL + `/user/${userId}/bookmarks/${bookmarkId}`);
-//     return response;
-// };
-
-// describe("Testing Get all bookmarked subchapters for a user by userId route", () => {
-//     it("should return empty data for user", async () => {
-//         const expectedResult = [];
-//         const res = await getBookmarks(NO_BOOKMARKS_USER_ID);
-//         expect(res.data).toEqual(expectedResult);
-//         // expect(res.status).toEqual(200)
-//     })
-
-//     it("should return all bookmark data for user", async () => {
-//         const res = await getBookmarks(WITH_BOOKMARKS_USER_ID);
-//         expect(res.status).toEqual(200);
+// describe("Testing objects ", () => {
+//     it("object assignment", () => {
+//         const data = { one: 1 };
+//         data["two"] = 2;
+//         expect(data).toEqual({ one: 1, two: 2 });
 //     })
 // })
+
+
+const axios = require('axios')
+
+// const NO_BOOKMARKS_USER_ID = "63e87aed80b6c0bcb29d15d4"
+// const WITH_BOOKMARKS_USER_ID = "63e87a7780b6c0bcb29d15d0"
+const CHAPTER_ID = "6415b83df79d4564b394fbea"
+const API_URL = "https://spark-sicu-proficiency-readiness-kit-backend.vercel.app"
+const NO_BOOKMARKS_USER_ID = "115440839276691517624"
+
+const getBookmarks = async (userId) => {
+    const response = await axios.get(API_URL + `/user/${userId}/bookmarks`);
+    return response;
+};
+
+const getAllSubchaptersForAChapter = async (userId, chapterId) => {
+    const response = await axios.get(API_URL + `/user/${userId}/bookmarks/chapters/${chapterId}`);
+    return response;
+};
+
+const addBookmarkForASubchapter = async (userId, data) => {
+    const response = await axios.put(API_URL + `/user/${userId}/bookmarks/`, data);
+    return response;
+};
+
+const deleteBookmarkForASubchapter = async (userId, bookmarkId) => {
+    const response = await axios.delete(API_URL + `/user/${userId}/bookmarks/${bookmarkId}`);
+    return response;
+};
+
+describe("Testing Get all bookmarked subchapters for a user by userId route", () => {
+    it("should return empty data for user", async () => {
+        const expectedResult = [];
+        const res = await getBookmarks(NO_BOOKMARKS_USER_ID);
+        expect(res.data).toEqual(expectedResult);
+        // expect(res.status).toEqual(200)
+    })
+
+    // it("should return all bookmark data for user", async () => {
+    //     const res = await getBookmarks(WITH_BOOKMARKS_USER_ID);
+    //     expect(res.status).toEqual(200);
+    // })
+})
 
 // describe("Testing Get all subchapters of a specified chapter for a user by userId route", () => {
 //     it("should return all the subchapters data for user", async () => {
@@ -79,10 +80,10 @@ describe("Testing objects ", () => {
 
 // })
 
-// // describe("Testing Delete Bookmark by BookmarkId and UserId route", () => {
-// //     it ("should return data for user id 63fb1b9e40628116ea970e57", async () => {
+// describe("Testing Delete Bookmark by BookmarkId and UserId route", () => {
+//     it ("should return data for user id 63fb1b9e40628116ea970e57", async () => {
 
-// //     const res = await deleteBookmarkForASubchapter(ONE_BOOKMARKS_USER_ID, addedBookmarkId);
-// //     expect(res.status).toEqual(200);
-// //     })
-// // })
+//     const res = await deleteBookmarkForASubchapter(ONE_BOOKMARKS_USER_ID, addedBookmarkId);
+//     expect(res.status).toEqual(200);
+//     })
+// })
