a354bdbae3586c2a9e8e38b557baab6b4576e2f8
Lin Peishan <linpeishann@gmail.com>
2023-04-03 18:40:56 +0800

    Update App.jsx

--- SPARK-App/src/App.jsx
+++ SPARK-App/src/App.jsx
@@ -23,10 +23,16 @@ function App() {
 
   useEffect(() => {
     getUser();
-    if (window.popupsBlocked) {
-      alert("Please enable pop-up blocker to continue.");
-    }
   }, [user])
+
+  useEffect(() => {
+    const popupWindow = window.open('', '_blank');
+    if (!popupWindow || popupWindow.closed || typeof popupWindow.closed === 'undefined') {
+      alert('Please enable pop-up blocker in Safari to use this feature.');
+    } else {
+      popupWindow.close();
+    }
+  }, []);
 
   if (!user && "user" in sessionStorage) {
     return (
